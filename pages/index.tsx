986c1a264dcbf1d86f5935dcd0da011ea47e0d6c
Dan Ward <danward@gmail.com>
2022-08-10 21:30:04 -0700

    Add UI to swap between user and staking wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,47 @@
+import classNames from "classnames";
 import ClientOnly from "features/client-only";
 import UserNftList from "features/user-nft-list";
 import type { NextPage } from "next";
+import { useState } from "react";
+
+enum WalletType {
+  STAKING = "STAKING",
+  USER = "USER",
+}
 
 const Home: NextPage = () => {
+  const [activeWallet, setActiveWallet] = useState<WalletType>(WalletType.USER);
+
   return (
     <div className="h-full w-full flex">
       <div className="max-w-5xl p-4 pt-8 text-2xl w-full">
         <ClientOnly>
-          <UserNftList />
+          <div className="flex pb-6 space-x-2">
+            <button
+              className={classNames({
+                "p-2 px-4 rounded  text-white uppercase text-xl pt-2.5": true,
+                "bg-green-800": activeWallet === WalletType.USER,
+                "bg-transparent border-2 border-green-800":
+                  activeWallet !== WalletType.USER,
+              })}
+              onClick={() => setActiveWallet(WalletType.USER)}
+            >
+              Wallet
+            </button>
+            <button
+              className={classNames({
+                "p-2 px-4 rounded text-white uppercase text-xl pt-2.5": true,
+                "bg-green-800": activeWallet === WalletType.STAKING,
+                "bg-transparent border-2 border-green-800":
+                  activeWallet !== WalletType.STAKING,
+              })}
+              onClick={() => setActiveWallet(WalletType.STAKING)}
+            >
+              Staked
+            </button>
+          </div>
+          {activeWallet === WalletType.USER && <UserNftList />}
+          {activeWallet === WalletType.STAKING && <div>Staked</div>}
         </ClientOnly>
       </div>
     </div>
